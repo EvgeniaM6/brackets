48d8cf355e4fe1e3b0d894f5dc091a4e03943f7d
EvgeniaM6 <evgminukova6@gmail.com>
2022-08-08 17:11:15 +0300

    feat: create function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let result
+
+  let openBrackets = []
+  openBrackets.push(...bracketsConfig.map((item) => (item[0])))
+
+  let bracketsPair = {}
+  bracketsConfig.forEach((item) => {
+    bracketsPair[item[1]] = item[0]
+  })
+
+  let stack = []
+  for (let i = 0; i < str.length; i++) {
+    const checkedBracket = str[i];
+    let lastStackBracket = stack[stack.length - 1]
+    
+    if (openBrackets.includes(checkedBracket)) {
+      if (checkedBracket !== bracketsPair[checkedBracket]) {
+        stack.push(checkedBracket)
+      } else {
+        if (checkedBracket === lastStackBracket) {
+          stack.pop()
+        } else {
+          stack.push(checkedBracket)
+        }
+      }
+
+    } else {
+      if (stack.length === 0) {
+        return false
+      }
+
+      if (bracketsPair[checkedBracket] === lastStackBracket) {
+        stack.pop()
+      } else {
+        return false
+      }
+    } 
+  }
+  return stack.length === 0
 }
